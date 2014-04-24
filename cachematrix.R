a491a017ab876a016c0348cd84df06e2005aee65
John S Johnson <spicetrder@aol.com>
2014-04-24 16:48:46 -0700

    Added functional R code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+##
+## This R.script provides two functions:
+## makeCacheMatrix - given an input matrix, provides functions for use elsewhere 
+## cacheSolve - returns matrix inverse of matrix input
+##
+##
+## This function will store a matrix and its' inverse and provide functions
+## to access matrix and inverse
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x) {
+  cachedMXT <- x
+  cachedINV <- solve(x)
+  #
+  getMTX <- function() x
+  getINV <- function() cachedINV
+list(getMTX = getMTX, getINV = getINV)
 }
-
-
-## Write a short comment describing this function
+##
+##
+## This function will take an input matrix created with the makeCacheMatrix function
+## and, if the inverse exists, return its' inverse, otherwise the inverse is calculated
+## within the function and returned.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getINV()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  INV <- solve(data, ...)
+  x$setINV(x)
+  INV
 }
+##
